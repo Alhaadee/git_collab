9d055730d2f6166055f78747bd4768948cfddd29
Alhaadee <alhaadee91@gmail.com>
2022-09-09 12:14:38 +0100

    adds pull message

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -3,5 +3,6 @@ public class Runner {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("we're working on the develop branch");
+        System.out.println("we're making a pull request");
     }
 }
